7f21c1475c07c7f0afb42f682c7bd8396bb251dd
Gnana-323 <126848913+Gnana-323@users.noreply.github.com>
2023-10-20 15:37:26 +0530

    Add files via upload

---
+++ encapsulation-java/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+        obj.setName("Prasanna");
+        System.out.println("Name:"+obj.getName());
+       // System.out.println(" Error: you might seen some error");
+    }
+}
